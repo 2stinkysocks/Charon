1e99fc0d780deccc612fac1f039d3e7661162e67
2stinkysocks <zmehall@gmail.com>
2020-12-17 08:14:44 -0700

    remove broken api latency option in -ping

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Get the bot's ping",
     async execute(message, args, client){
         const m = await message.channel.send(`Waiting..`);
-        message.channel.send(`Done! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${client.ping}ms`);
+        message.channel.send(`Done! Latency is ${m.createdTimestamp - message.createdTimestamp}ms.`);
       	m.delete();
     }
 }
