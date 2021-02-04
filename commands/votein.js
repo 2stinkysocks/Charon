27a8c219e8a191d1563ae1db709c06497955e7fc
2stinkysocks <zmehall@gmail.com>
2021-02-04 16:51:51 -0700

    fix error in votein and backup obols data

--- commands/votein.js
+++ commands/votein.js
@@ -50,42 +50,42 @@ module.exports = {
         break;
     }
     message.channel.send("**Note:** Lists that aren't displayed have 0 members.");
-    if(message.guild.roles.cache.find(role => role.name == `vc list`).members > 0) message.channel.send({
+    if(message.guild.roles.cache.find(role => role.name == `vc list`).members.size > 0) message.channel.send({
       embed: {
         color: 4360181,
         title: `VC list`,
         description: message.guild.roles.cache.find(role => role.name == `vc list`).members.map(m => m.user.tag).join('\n')
       }
     });
-    if(message.guild.roles.cache.find(role => role.name == `sos list`).members > 0) message.channel.send({
+    if(message.guild.roles.cache.find(role => role.name == `sos list`).members.size > 0) message.channel.send({
       embed: {
         color: 4360181,
         title: `SOS list`,
         description: message.guild.roles.cache.find(role => role.name == `sos list`).members.map(m => m.user.tag).join('\n')
       }
     });
-    if(message.guild.roles.cache.find(role => role.name == `rsvd list`).members > 0) message.channel.send({
+    if(message.guild.roles.cache.find(role => role.name == `rsvd list`).members.size > 0) message.channel.send({
       embed: {
         color: 4360181,
         title: `RSVD list`,
         description: message.guild.roles.cache.find(role => role.name == `rsvd list`).members.map(m => m.user.tag).join('\n')
       }
     });
-    if(message.guild.roles.cache.find(role => role.name == `void list`).members > 0) message.channel.send({
+    if(message.guild.roles.cache.find(role => role.name == `void list`).members.size > 0) message.channel.send({
       embed: {
         color: 4360181,
         title: `Void list`,
         description: message.guild.roles.cache.find(role => role.name == `void list`).members.map(m => m.user.tag).join('\n')
       }
     });
-    if(message.guild.roles.cache.find(role => role.name == `allies list`).members > 0) message.channel.send({
+    if(message.guild.roles.cache.find(role => role.name == `allies list`).members.size > 0) message.channel.send({
       embed: {
         color: 4360181,
         title: `Allies list`,
         description: message.guild.roles.cache.find(role => role.name == `allies list`).members.map(m => m.user.tag).join('\n')
       }
     });
-    if(message.guild.roles.cache.find(role => role.name == `fill list`).members > 0) message.channel.send({
+    if(message.guild.roles.cache.find(role => role.name == `fill list`).members.size > 0) message.channel.send({
       embed: {
         color: 4360181,
         title: `Fill list`,
