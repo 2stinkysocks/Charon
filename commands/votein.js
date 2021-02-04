b7c885ab09976f372b678a4492fd927a95e08760
2stinkysocks <zmehall@gmail.com>
2021-02-04 16:49:51 -0700

    shrink votein code

--- commands/votein.js
+++ commands/votein.js
@@ -2,11 +2,11 @@ module.exports = {
   name: 'votein',
   description: 'Vote into a white star list',
   oneListOnly: false, // used to toggle if -votein will always vote into sos
-  async execute(message, args) {
+  execute(message, args) {
     if(this.oneListOnly) {
       if (message.member.roles.cache.find(role => role.name == `sos list`)) return message.channel.send(`You have voted into sos already!`);
       message.channel.send("**NOTE**\n\nEveryone will be voted into the same list this week.")
-      await message.member.roles.add(message.guild.roles.cache.find(role => role.name == `sos list`)).catch(console.error);
+      message.member.roles.add(message.guild.roles.cache.find(role => role.name == `sos list`)).catch(console.error);
       message.channel.send({
         embed: {
           color: 4360181,
@@ -20,7 +20,7 @@ module.exports = {
     if (message.channel.name != `white-star-enlist` && message.channel.name != `private-bot-playground`) return message.channel.send(`You can't enlist in this channel!`);
     if (args[0] == null) return message.channel.send(`You need to specify a list! (vc, sos, rsvd, allies, fill)`);
     var list = args[0].toLowerCase();
-    if (list != `vc` && list != `sos` && list != `rsvd` && list != `void` && list != `allies` && list != `fill`) return message.channel.send(`${args[0]} is not a valid list! use ${config.prefix}votein (vc, sos, rsvd, void, allies, fill)`);
+    if (list != `vc` && list != `sos` && list != `rsvd` && list != `void` && list != `allies` && list != `fill`) return message.channel.send(`${args[0]} is not a valid list! Use ${config.prefix}votein (vc, sos, rsvd, void, allies, fill)`);
     if (message.member.roles.cache.find(role => role.name == `vc list`)) return message.channel.send(`You have voted into vc already!`);
     if (message.member.roles.cache.find(role => role.name == `sos list`)) return message.channel.send(`You have voted into sos already!`);
     if (message.member.roles.cache.find(role => role.name == `rsvd list`)) return message.channel.send(`You have voted into rsvd already!`);
@@ -30,290 +30,68 @@ module.exports = {
     switch (list) {
       case `vc`:
         var role = message.guild.roles.cache.find(role => role.name == `vc list`);
-        await message.member.roles.add(role).catch(console.error);
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `VC list`,
-            description: message.guild.roles.cache.find(role => role.name == `vc list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `SOS list`,
-            description: message.guild.roles.cache.find(role => role.name == `sos list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `RSVD list`,
-            description: message.guild.roles.cache.find(role => role.name == `rsvd list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Void list`,
-            description: message.guild.roles.cache.find(role => role.name == `void list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Allies list`,
-            description: message.guild.roles.cache.find(role => role.name == `allies list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Fill list`,
-            description: message.guild.roles.cache.find(role => role.name == `fill list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-
-        message.channel.send(`${message.author.username} has signed up for this week's white star list.`);
+        message.member.roles.add(role).catch(console.error);
         break;
       case `sos`:
         var role = message.guild.roles.cache.find(role => role.name == `sos list`);
-        await message.member.roles.add(role).catch(console.error);
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `VC list`,
-            description: message.guild.roles.cache.find(role => role.name == `vc list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `SOS list`,
-            description: message.guild.roles.cache.find(role => role.name == `sos list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `RSVD list`,
-            description: message.guild.roles.cache.find(role => role.name == `rsvd list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Void list`,
-            description: message.guild.roles.cache.find(role => role.name == `void list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Allies list`,
-            description: message.guild.roles.cache.find(role => role.name == `allies list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Fill list`,
-            description: message.guild.roles.cache.find(role => role.name == `fill list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-
-        message.channel.send(`${message.author.username} has signed up for this week's white star list.`);
+        message.member.roles.add(role).catch(console.error);
         break;
       case `rsvd`:
         var role = message.guild.roles.cache.find(role => role.name == `rsvd list`);
-        await message.member.roles.add(role).catch(console.error);
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `VC list`,
-            description: message.guild.roles.cache.find(role => role.name == `vc list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `SOS list`,
-            description: message.guild.roles.cache.find(role => role.name == `sos list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `RSVD list`,
-            description: message.guild.roles.cache.find(role => role.name == `rsvd list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Void list`,
-            description: message.guild.roles.cache.find(role => role.name == `void list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Allies list`,
-            description: message.guild.roles.cache.find(role => role.name == `allies list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Fill list`,
-            description: message.guild.roles.cache.find(role => role.name == `fill list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-
-        message.channel.send(`${message.author.username} has signed up for this week's white star list.`);
+        message.member.roles.add(role).catch(console.error);
         break;
-        case `void`:
-          var role = message.guild.roles.cache.find(role => role.name == `void list`);
-          await message.member.roles.add(role).catch(console.error);
-          message.channel.send({
-            embed: {
-              color: 4360181,
-              title: `VC list`,
-              description: message.guild.roles.cache.find(role => role.name == `vc list`).members.map(m => m.user.tag).join('\n')
-            }
-          });
-          message.channel.send({
-            embed: {
-              color: 4360181,
-              title: `SOS list`,
-              description: message.guild.roles.cache.find(role => role.name == `sos list`).members.map(m => m.user.tag).join('\n')
-            }
-          });
-          message.channel.send({
-            embed: {
-              color: 4360181,
-              title: `RSVD list`,
-              description: message.guild.roles.cache.find(role => role.name == `rsvd list`).members.map(m => m.user.tag).join('\n')
-            }
-          });
-          message.channel.send({
-            embed: {
-              color: 4360181,
-              title: `Void list`,
-              description: message.guild.roles.cache.find(role => role.name == `void list`).members.map(m => m.user.tag).join('\n')
-            }
-          });
-          message.channel.send({
-            embed: {
-              color: 4360181,
-              title: `Allies list`,
-              description: message.guild.roles.cache.find(role => role.name == `allies list`).members.map(m => m.user.tag).join('\n')
-            }
-          });
-          message.channel.send({
-            embed: {
-              color: 4360181,
-              title: `Fill list`,
-              description: message.guild.roles.cache.find(role => role.name == `fill list`).members.map(m => m.user.tag).join('\n')
-            }
-          });
-  
-          message.channel.send(`${message.author.username} has signed up for this week's white star list.`);
-          break;
       case `allies`:
         var role = message.guild.roles.cache.find(role => role.name == `allies list`);
-        await message.member.roles.add(role).catch(console.error);
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `VC list`,
-            description: message.guild.roles.cache.find(role => role.name == `vc list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `SOS list`,
-            description: message.guild.roles.cache.find(role => role.name == `sos list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `RSVD list`,
-            description: message.guild.roles.cache.find(role => role.name == `rsvd list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Void list`,
-            description: message.guild.roles.cache.find(role => role.name == `void list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Allies list`,
-            description: message.guild.roles.cache.find(role => role.name == `allies list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Fill list`,
-            description: message.guild.roles.cache.find(role => role.name == `fill list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
+        message.member.roles.add(role).catch(console.error);
         break;
       case `fill`:
         var role = message.guild.roles.cache.find(role => role.name == `fill list`);
-        await message.member.roles.add(role).catch(console.error);
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `VC list`,
-            description: message.guild.roles.cache.find(role => role.name == `vc list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `SOS list`,
-            description: message.guild.roles.cache.find(role => role.name == `sos list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `RSVD list`,
-            description: message.guild.roles.cache.find(role => role.name == `rsvd list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Void list`,
-            description: message.guild.roles.cache.find(role => role.name == `void list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Allies list`,
-            description: message.guild.roles.cache.find(role => role.name == `allies list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-        message.channel.send({
-          embed: {
-            color: 4360181,
-            title: `Fill list`,
-            description: message.guild.roles.cache.find(role => role.name == `fill list`).members.map(m => m.user.tag).join('\n')
-          }
-        });
-
-        message.channel.send(`${message.author.username} has signed up for this week's white star list.`);
+        message.member.roles.add(role).catch(console.error);
         break;
     }
+    message.channel.send("**Note:** Lists that aren't displayed have 0 members.");
+    if(message.guild.roles.cache.find(role => role.name == `vc list`).members > 0) message.channel.send({
+      embed: {
+        color: 4360181,
+        title: `VC list`,
+        description: message.guild.roles.cache.find(role => role.name == `vc list`).members.map(m => m.user.tag).join('\n')
+      }
+    });
+    if(message.guild.roles.cache.find(role => role.name == `sos list`).members > 0) message.channel.send({
+      embed: {
+        color: 4360181,
+        title: `SOS list`,
+        description: message.guild.roles.cache.find(role => role.name == `sos list`).members.map(m => m.user.tag).join('\n')
+      }
+    });
+    if(message.guild.roles.cache.find(role => role.name == `rsvd list`).members > 0) message.channel.send({
+      embed: {
+        color: 4360181,
+        title: `RSVD list`,
+        description: message.guild.roles.cache.find(role => role.name == `rsvd list`).members.map(m => m.user.tag).join('\n')
+      }
+    });
+    if(message.guild.roles.cache.find(role => role.name == `void list`).members > 0) message.channel.send({
+      embed: {
+        color: 4360181,
+        title: `Void list`,
+        description: message.guild.roles.cache.find(role => role.name == `void list`).members.map(m => m.user.tag).join('\n')
+      }
+    });
+    if(message.guild.roles.cache.find(role => role.name == `allies list`).members > 0) message.channel.send({
+      embed: {
+        color: 4360181,
+        title: `Allies list`,
+        description: message.guild.roles.cache.find(role => role.name == `allies list`).members.map(m => m.user.tag).join('\n')
+      }
+    });
+    if(message.guild.roles.cache.find(role => role.name == `fill list`).members > 0) message.channel.send({
+      embed: {
+        color: 4360181,
+        title: `Fill list`,
+        description: message.guild.roles.cache.find(role => role.name == `fill list`).members.map(m => m.user.tag).join('\n')
+      }
+    });
+
   }
 }
